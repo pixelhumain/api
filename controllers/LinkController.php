9eab9fe523492c2ab84a8b3bb16b2c1c9e055a6c
raphael <raphael_riviere@yahoo.fr>
2016-08-16 17:31:55 +0400

    Ajout du controller link

---
+++ controllers/LinkController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * LinkController.php
+ *
+ * Manage Links between Organization, Person, Projet and Event
+ *
+ * Date: 16/08/2016
+ */
+class LinkController extends CommunecterController {
+
+	protected function beforeAction($action) {
+		parent::initPage();
+		return parent::beforeAction($action);
+	}
+	public function actions()
+	{
+	    return array(
+	        /*'removemember'     	=> 'citizenToolKit.controllers.link.RemoveMemberAction',
+	        "removerole"		=> 'citizenToolKit.controllers.link.RemoveRoleAction',
+			'removecontributor' => 'citizenToolKit.controllers.link.RemoveContributorAction',
+			
+			'validate' 			=> 'citizenToolKit.controllers.link.ValidateAction',*/
+			//New Actions
+			
+			'follow' 			=> 'citizenToolKit.controllers.link.FollowAction',
+			'connect' 			=> 'citizenToolKit.controllers.link.ConnectAction',
+			'disconnect' 		=> 'citizenToolKit.controllers.link.DisconnectAction',
+			
+	    );
+	}
+}
+
+?>
