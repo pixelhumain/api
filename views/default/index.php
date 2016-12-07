13b87ba8e263b35b0aad7f84182d4ce8d9c3a7a1
Raphael <raphael_riviere@yahoo.fr>
2016-12-07 09:27:15 +0400

    Modif API

--- views/default/index.php
+++ views/default/index.php
@@ -1,138 +1,74 @@
 <?php 
 	$cssAnsScriptFilesModule = array(
-	  //l'ordre est a respecter
-		'/plugins/swagger-ui/css/typography.css',
-	  '/plugins/swagger-ui/css/reset.css',
-	  '/plugins/swagger-ui/css/screen.css',
-	  //'/plugins/swagger-ui/css/print.css',
+    //l'ordre est a respecter
+    '/plugins/swagger-ui/css/reset.css',
+    '/plugins/swagger-ui/css/screen.css',
+    //'/plugins/swagger-ui/css/print.css',
     '/plugins/swagger-ui/lib/jquery.slideto.min.js',
     '/plugins/swagger-ui/lib/jquery.wiggle.min.js',
-    //'/plugins/swagger-ui/jquery.ba-bbq.min.js',
-	  '/plugins/swagger-ui/lib/handlebars-2.0.0.js',
-	  '/plugins/swagger-ui/lib/underscore-min.js',
-	  '/plugins/swagger-ui/lib/backbone-min.js',
-	  '/plugins/swagger-ui/swagger-ui.min.js',
-	  '/plugins/swagger-ui/lib/highlight.7.3.pack.js',
-	  '/plugins/swagger-ui/lib/jsoneditor.min.js',
-	  '/plugins/swagger-ui/lib/marked.js',
-	  '/plugins/swagger-ui/lib/swagger-oauth.js'
-
-
-	/*
-
-	<link href='css/typography.css' media='screen' rel='stylesheet' type='text/css'/>
-  <link href='css/reset.css' media='screen' rel='stylesheet' type='text/css'/>
-  <link href='css/screen.css' media='screen' rel='stylesheet' type='text/css'/>
-  <link href='css/reset.css' media='print' rel='stylesheet' type='text/css'/>
-  <link href='css/print.css' media='print' rel='stylesheet' type='text/css'/>
-	<script src='lib/jquery-1.8.0.min.js' type='text/javascript'></script>
-  <script src='lib/jquery.slideto.min.js' type='text/javascript'></script>
-  <script src='lib/jquery.wiggle.min.js' type='text/javascript'></script>
-  <script src='lib/jquery.ba-bbq.min.js' type='text/javascript'></script>
-  <script src='lib/handlebars-2.0.0.js' type='text/javascript'></script>
-  <script src='lib/underscore-min.js' type='text/javascript'></script>
-  <script src='lib/backbone-min.js' type='text/javascript'></script>
-  <script src='swagger-ui.js' type='text/javascript'></script>
-  <script src='lib/highlight.7.3.pack.js' type='text/javascript'></script>
-  <script src='lib/jsoneditor.min.js' type='text/javascript'></script>
-  <script src='lib/marked.js' type='text/javascript'></script>
-  <script src='lib/swagger-oauth.js' type='text/javascript'></script>*/
-
+    '/plugins/swagger-ui/lib/jquery.ba-bbq.min.js',
+    '/plugins/swagger-ui/lib/handlebars-4.0.5.js',
+    '/plugins/swagger-ui/lib/lodash.min.js',
+    '/plugins/swagger-ui/lib/backbone-min.js',  
+    '/plugins/swagger-ui/swagger-ui.min.js',
+    '/plugins/swagger-ui/lib/highlight.9.1.0.pack.js',
+    '/plugins/swagger-ui/lib/highlight.9.1.0.pack_extended.js',
+    '/plugins/swagger-ui/lib/jsoneditor.min.js',
+    '/plugins/swagger-ui/lib/marked.js',
+    '/plugins/swagger-ui/lib/swagger-oauth.js'
 	);
 	HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesModule,Yii::app()->request->baseUrl);
 
 ?>
-<div id='header'>
-  <div class="swagger-ui-wrap">
-    <a id="logo" href="http://swagger.io">swagger</a>
-    <form id='api_selector'>
-      <div class='input'><input placeholder="api_key" id="input_apiKey" name="apiKey" type="text"/></div>
-      <div class='input'><a id="explore" href="#" data-sw-translate>Explore</a></div>
-    </form>
-  </div>
-</div>
 <div id="message-bar" class="swagger-ui-wrap" data-sw-translate>&nbsp;</div>
 <div id="swagger-ui-container" class="swagger-ui-wrap"></div>
 <?php
-
     $server = ((isset($_SERVER['HTTPS']) AND (!empty($_SERVER['HTTPS'])) AND strtolower($_SERVER['HTTPS'])!='off') ? 'https://' : 'http://').$_SERVER['HTTP_HOST'];
     $url = $server.Yii::app()->createUrl("/api/data/get/");
 ?>
 <script type="text/javascript">
-
-	   var url = "<?php echo $url ; ?>"
+var url = "<?php echo $url ; ?>"
 jQuery(document).ready(function() {
+  hljs.configure({
+    highlightSizeThreshold: 5000
+  });
 
-
-
-      // Pre load translate...
+  // Pre load translate...
+  if(window.SwaggerTranslator) {
+    window.SwaggerTranslator.translate();
+  }
+  window.swaggerUi = new SwaggerUi({
+    url: url,
+    dom_id: "swagger-ui-container",
+    supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
+    onComplete: function(swaggerApi, swaggerUi){
+      if(typeof initOAuth == "function") {
+        initOAuth({
+          clientId: "your-client-id",
+          clientSecret: "your-client-secret-if-required",
+          realm: "your-realms",
+          appName: "your-app-name",
+          scopeSeparator: " ",
+          additionalQueryStringParams: {}
+        });
+      }
       if(window.SwaggerTranslator) {
         window.SwaggerTranslator.translate();
       }
-      window.swaggerUi = new SwaggerUi({
-        url: url,
-        dom_id: "swagger-ui-container",
-        supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
-        onComplete: function(swaggerApi, swaggerUi){
-          if(typeof initOAuth == "function") {
-            initOAuth({
-              clientId: "your-client-id",
-              clientSecret: "your-client-secret-if-required",
-              realm: "your-realms",
-              appName: "your-app-name", 
-              scopeSeparator: ",",
-              additionalQueryStringParams: {}
-            });
-          }
-
-          if(window.SwaggerTranslator) {
-            window.SwaggerTranslator.translate();
-          }
-
-          $('pre code').each(function(i, e) {
-            hljs.highlightBlock(e)
-          });
-
-          addApiKeyAuthorization();
-        },
-        onFailure: function(data) {
-          log("Unable to Load SwaggerUI");
-        },
-        docExpansion: "none",
-        jsonEditor: true,
-        apisSorter: "alpha",
-        defaultModelRendering: 'schema',
-        showRequestHeaders: true
-      });
-
-      
-
-      $('#input_apiKey').change(addApiKeyAuthorization);
-
-      // if you have an apiKey you would like to pre-populate on the page for demonstration purposes...
-      /*
-        var apiKey = "myApiKeyXXXX123456789";
-        $('#input_apiKey').val(apiKey);
-      */
-
-      window.swaggerUi.load();
-
-      
+    },
+    onFailure: function(data) {
+      log("Unable to Load SwaggerUI");
+    },
+    docExpansion: "none",
+    jsonEditor: false,
+    defaultModelRendering: 'schema',
+    showRequestHeaders: false
   });
-
-function addApiKeyAuthorization(){
-        var key = encodeURIComponent($('#input_apiKey')[0].value);
-        if(key && key.trim() != "") {
-            var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization("api_key", key, "query");
-            window.swaggerUi.api.clientAuthorizations.add("api_key", apiKeyAuth);
-            log("added key " + key);
-        }
-      }
-
+  window.swaggerUi.load();
+});
 function log() {
-        if ('console' in window) {
-          console.log.apply(console, arguments);
-        }
-      }
-
-  </script>
+  if ('console' in window) {
+    console.log.apply(console, arguments);
+  }
+}
+</script>
